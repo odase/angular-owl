8502727a10620233fb3ae6fd6b9f53312a06d74e
Mileem <okarina71@yahoo.fr>
2017-07-28 12:10:05 +0200

    Adding input text component

--- src/angular-owl-component.module.ts
+++ src/angular-owl-component.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MdButtonToggleModule } from '@angular/material';
-import { AngularOwlToggleComponent } from './angular-owl-toggle.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import { AngularOwlToggleComponent } from './angular-owl-toggle.component';
+import { AngularOwlInputTextComponent } from './angular-owl-input-text.component';
+
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+} from '@angular/material';
 
 @NgModule({
   imports: [
       CommonModule,
+      BrowserAnimationsModule,
+      MdInputModule,
       MdButtonToggleModule,
   ],
-  exports: [ AngularOwlToggleComponent ],
+  exports: [ AngularOwlToggleComponent, AngularOwlInputTextComponent ],
   declarations: [
       AngularOwlToggleComponent,
+      AngularOwlInputTextComponent
   ]
 })
 export class AngularOwlComponentModule { }
---
+++ src/angular-owl-input-text.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup }        from '@angular/forms';
+
+@Component({
+  selector: 'ao-input-text',
+  template: `<md-input-container class="example-full-width">
+      <input mdInput [placeholder]="question"
+      [formControl]="form.controls[predicate]"
+      [(ngModel)]="subject[predicate]">
+      <md-error *ngIf="form.controls[predicate].hasError('pattern')">
+          Please enter a valid item
+      </md-error>
+      <md-error *ngIf="form.controls[predicate].hasError('required')">
+          {{ question }} is <strong>required</strong>
+      </md-error>
+  </md-input-container>`,
+  styleUrls: []
+})
+export class AngularOwlInputTextComponent implements OnInit {
+    @Input() form: FormGroup;
+    @Input() subject = <Object>null;
+    @Input() predicate = <String>null;
+    @Input() question = <String>null;
+
+  ngOnInit() {
+  }
+
+}
