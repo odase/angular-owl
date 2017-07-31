ec69c2e92e0591a4607262eb04b75c2a3c6fc011
Mileem <okarina71@yahoo.fr>
2017-07-31 15:21:51 +0200

    Checks if inputs are valid

--- src/angular-owl-input-text.component.ts
+++ src/angular-owl-input-text.component.ts
@@ -3,17 +3,17 @@ import { FormGroup }        from '@angular/forms';
 
 @Component({
   selector: 'ao-input-text',
-  template: `<md-input-container class="example-full-width">
-      <input mdInput [placeholder]="question"
-      [formControl]="form.controls[predicate]"
-      [(ngModel)]="subject[predicate]">
-      <md-error *ngIf="form.controls[predicate].hasError('pattern')">
-          Please enter a valid item
-      </md-error>
-      <md-error *ngIf="form.controls[predicate].hasError('required')">
-          {{ question }} is <strong>required</strong>
-      </md-error>
-  </md-input-container>`,
+  template: `<md-input-container  *ngIf="form && form?.controls[predicate] && subject" class="example-full-width">
+    <input mdInput [placeholder]="question"
+    [formControl]="form?.controls[predicate]"
+    [(ngModel)]="subject[predicate]">
+    <md-error *ngIf="form?.controls[predicate].hasError('pattern')">
+        Please enter a valid item
+    </md-error>
+    <md-error *ngIf="form?.controls[predicate].hasError('required')">
+        {{ question }} is <strong>required</strong>
+    </md-error>
+</md-input-container>`,
   styleUrls: []
 })
 export class AngularOwlInputTextComponent implements OnInit {
