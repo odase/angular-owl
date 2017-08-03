8e81585bf9e9e3fdbab2f3238d1891053d9986c6
Mileem <okarina71@yahoo.fr>
2017-08-03 15:18:41 +0200

    Remove <form>

--- src/angular-owl-input-tel.component.ts
+++ src/angular-owl-input-tel.component.ts
@@ -3,8 +3,7 @@ import { FormGroup }        from '@angular/forms';
 
 @Component({
   selector: 'ao-input-tel',
-  template: `<form class="example-form">
-  <md-input-container class="example-full-width">
+  template: `<md-input-container class="example-full-width">
     <input type="tel" mdInput pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
     [placeholder]="question"
     [formControl]="form?.controls[predicate]"
@@ -16,8 +15,7 @@ import { FormGroup }        from '@angular/forms';
     <md-error *ngIf="form?.controls[predicate].hasError('required')">
         {{ question }} is <strong>required</strong>
     </md-error>
-  </md-input-container>
-</form>`,
+  </md-input-container>`,
   styleUrls: []
 })
 export class AngularOwlInputTelComponent implements OnInit {
