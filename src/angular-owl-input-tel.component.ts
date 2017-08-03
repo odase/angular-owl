bd89b1e3911997c9891b0e8372b2e0876c0f8623
Mileem <okarina71@yahoo.fr>
2017-08-03 16:00:20 +0200

    Adding verification

--- src/angular-owl-input-tel.component.ts
+++ src/angular-owl-input-tel.component.ts
@@ -3,18 +3,18 @@ import { FormGroup }        from '@angular/forms';
 
 @Component({
   selector: 'ao-input-tel',
-  template: `<md-input-container class="example-full-width">
-    <input type="tel" mdInput pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
-    [placeholder]="question"
-    [formControl]="form?.controls[predicate]"
-    [(ngModel)]="subject[predicate]">
-    <md-icon mdSuffix>phone</md-icon>
-    <md-error *ngIf="form?.controls[predicate].hasError('pattern')">
-        Please enter a valid phone number
-    </md-error>
-    <md-error *ngIf="form?.controls[predicate].hasError('required')">
-        {{ question }} is <strong>required</strong>
-    </md-error>
+  template: ` <md-input-container  *ngIf="form && form?.controls[predicate] && subject" class="example-full-width">
+      <input type="tel" mdInput [placeholder]="question"
+      pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
+      [formControl]="form?.controls[predicate]"
+      [(ngModel)]="subject[predicate]">
+      <md-icon mdSuffix>phone</md-icon>
+      <md-error *ngIf="form?.controls[predicate].hasError('pattern')">
+          Please enter a valid phone number
+      </md-error>
+      <md-error *ngIf="form?.controls[predicate].hasError('required')">
+          {{ question }} is <strong>required</strong>
+      </md-error>
   </md-input-container>`,
   styleUrls: []
 })
